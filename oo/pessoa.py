bda694f20a8c93af898a378f26119b3c7b6a96df
luiscascelli <luis.cascelli@gmail.com>
2019-12-10 16:22:36 -0200

    criado metodos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self): #metodo
         return f'Olá {id(self)}'
 
+    @staticmethod #@ = decorator
+    def metodo_estatico(): #funcao de classe, funcao atrelada à classe Pessoa
+        return 42
+    @classmethod #acessar dados da propria classe
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
 
 if __name__ == '__main__':
     luis = Pessoa('Luis')
@@ -17,3 +25,5 @@ if __name__ == '__main__':
     print (id(luis))
     print(luis.cumprimentar())
     print(antonio.filhos)
+    print (Pessoa.metodo_estatico(),luis.metodo_estatico())
+    print (Pessoa.nome_e_atributos_de_classe(), luis.nome_e_atributos_de_classe())
